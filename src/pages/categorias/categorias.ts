00928cea65dbeaf66cabcdc8885fbb7ade403cad
ealvess <everton10alves@gmail.com>
2020-04-03 21:03:43 -0400

    Mostrandos as cataegorias

--- src/pages/categorias/categorias.ts
+++ src/pages/categorias/categorias.ts
@@ -1,6 +1,8 @@
+import { API_CONFIG } from './../../config/api.config';
 import { CategoriaService } from './../../services/domain/categoria.service';
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { CategoriaDTO } from '../../models/categoria.dto';
 
 /**
  * Generated class for the CategoriasPage page.
@@ -16,6 +18,9 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class CategoriasPage {
 
+  bucketUrl: string = API_CONFIG.backetuBaseUrl;
+  items: CategoriaDTO[];
+
   constructor(public navCtrl: NavController, public navParams: NavParams,
     public categoriaService: CategoriaService) {
   }
@@ -23,6 +28,7 @@ export class CategoriasPage {
   ionViewDidLoad() {
     this.categoriaService.findAll()
       .subscribe(response => {
+        this.items = response;
         console.log(response);
       },
       error => {
