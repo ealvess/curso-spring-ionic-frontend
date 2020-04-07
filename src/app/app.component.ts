b27c91a8c3ac8a4e799d5208a8c67f6da617c377
ealvess <everton10alves@gmail.com>
2020-04-06 21:02:59 -0400

    Acesso ao carrinho: botao flutuante e menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,8 +15,8 @@ export class MyApp {
   pages: Array<{title: string, component: string}>;
 
   constructor(
-    public platform: Platform, 
-    public statusBar: StatusBar, 
+    public platform: Platform,
+    public statusBar: StatusBar,
     public splashScreen: SplashScreen,
     public auth: AuthService
   ) {
@@ -26,6 +26,7 @@ export class MyApp {
     this.pages = [
       { title: 'Profile', component: 'ProfilePage' },
       { title: 'Categorias', component: 'CategoriasPage' },
+      { title: 'Carrinho', component: 'CartPage'},
       { title: 'Logout', component: ''}
     ];
 
