65bf99f9d11a4225ba0503888226b0e2e9404736
ealvess <everton10alves@gmail.com>
2020-05-03 16:44:22 -0400

    Refresher

--- src/pages/produtos/produtos.ts
+++ src/pages/produtos/produtos.ts
@@ -12,7 +12,7 @@ import { LoadingController } from 'ionic-angular/components/loading/loading-cont
 })
 export class ProdutosPage {
 
-  items: ProdutoDTO[];
+  items : ProdutoDTO[];
 
   constructor(
     public navCtrl: NavController,
@@ -22,6 +22,10 @@ export class ProdutosPage {
   }
 
   ionViewDidLoad() {
+    this.loadData();
+  }
+
+  loadData() {
     let categoria_id = this.navParams.get('categoria_id');
     let loader = this.presentLoading();
     this.produtoService.findByCategoria(categoria_id)
@@ -30,24 +34,24 @@ export class ProdutosPage {
         loader.dismiss();
         this.loadImageUrls();
       },
-        error => {
-          loader.dismiss();
-        });
+      error => {
+        loader.dismiss();
+      });
   }
 
   loadImageUrls() {
-    for (var i = 0; i < this.items.length; i++) {
+    for (var i=0; i<this.items.length; i++) {
       let item = this.items[i];
       this.produtoService.getSmallImageFromBucket(item.id)
         .subscribe(response => {
           item.imageUrl = `${API_CONFIG.bucketBaseUrl}/prod${item.id}-small.jpg`;
         },
-          error => { });
+        error => {});
     }
   }
 
-  showDetail(produto_id: string) {
-    this.navCtrl.push('ProdutoDetailPage', { produto_id: produto_id });
+  showDetail(produto_id : string) {
+    this.navCtrl.push('ProdutoDetailPage', {produto_id: produto_id});
   }
 
   presentLoading() {
@@ -57,5 +61,12 @@ export class ProdutosPage {
     });
     loader.present();
     return loader;
+  }
+
+  doRefresh(refresher) {
+    this.loadData();
+    setTimeout(() => {
+      refresher.complete();
+    }, 1000);
   }
 }
