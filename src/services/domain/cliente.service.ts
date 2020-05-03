4e5d7e717aa2f17f65739c39ff4e691fa14f9bf0
ealvess <everton10alves@gmail.com>
2020-05-03 16:05:03 -0400

    Tela de confirmacao de pedido

--- src/services/domain/cliente.service.ts
+++ src/services/domain/cliente.service.ts
@@ -8,26 +8,30 @@ import { StorageService } from "../storage.service";
 @Injectable()
 export class ClienteService {
 
-    constructor(public http: HttpClient, public storage: StorageService) {
-    }
+  constructor(public http: HttpClient, public storage: StorageService) {
+  }
 
-    findByEmail(email: string){
-        return this.http.get(`${API_CONFIG.baseUrl}/clientes/email?value=${email}`);
-    }
+  findByEmail(email: string) {
+    return this.http.get(`${API_CONFIG.baseUrl}/clientes/email?value=${email}`);
+  }
 
-    getImageFromBucket(id : string) : Observable<any> {
-        let url = `${API_CONFIG.bucketBaseUrl}/cp${id}.jpg`
-        return this.http.get(url, {responseType : 'blob'});
-    }
+  findById(id: string) {
+    return this.http.get(`${API_CONFIG.baseUrl}/clientes/${id}`);
+  }
 
-    insert(obj : ClienteDTO) {
-      return this.http.post(
-          `${API_CONFIG.baseUrl}/clientes`,
-          obj,
-          {
-              observe: 'response',
-              responseType: 'text'
-          }
-      );
+  getImageFromBucket(id: string): Observable<any> {
+    let url = `${API_CONFIG.bucketBaseUrl}/cp${id}.jpg`
+    return this.http.get(url, { responseType: 'blob' });
+  }
+
+  insert(obj: ClienteDTO) {
+    return this.http.post(
+      `${API_CONFIG.baseUrl}/clientes`,
+      obj,
+      {
+        observe: 'response',
+        responseType: 'text'
+      }
+    );
   }
 }
