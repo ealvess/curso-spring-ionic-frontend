b56123dadfa4c343176738f2a0eb81ce6382f278
ealvess <everton10alves@gmail.com>
2020-05-03 18:54:29 -0400

    Fazendo upload da foto

--- src/services/domain/cliente.service.ts
+++ src/services/domain/cliente.service.ts
@@ -1,3 +1,4 @@
+import { ImageUtilService } from './../image-util.service';
 import { Injectable } from "@angular/core";
 import { HttpClient, HttpHeaders } from "@angular/common/http";
 import { Observable } from "rxjs/Rx";
@@ -8,8 +9,11 @@ import { StorageService } from "../storage.service";
 @Injectable()
 export class ClienteService {
 
-  constructor(public http: HttpClient, public storage: StorageService) {
-  }
+  constructor(
+    public http: HttpClient,
+    public storage: StorageService,
+    public imageUtilService: ImageUtilService) {
+}
 
   findByEmail(email: string) {
     return this.http.get(`${API_CONFIG.baseUrl}/clientes/email?value=${email}`);
@@ -34,4 +38,18 @@ export class ClienteService {
       }
     );
   }
+
+  uploadPicture(picture) {
+    let pictureBlob = this.imageUtilService.dataUriToBlob(picture);
+    let formData : FormData = new FormData();
+    formData.set('file', pictureBlob, 'file.png');
+    return this.http.post(
+        `${API_CONFIG.baseUrl}/clientes/picture`,
+        formData,
+        {
+            observe: 'response',
+            responseType: 'text'
+        }
+    );
+}
 }
